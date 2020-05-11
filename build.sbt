31dbfc4587cc2feb362aa11d257c2eec0df13022
Simon Roberts <simon@dancingcloudservices.com>
2020-05-11 11:41:33 -0600

    Initial

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "IntroScalaClass"
+
+version := "0.1"
+
+scalaVersion := "2.12.11"
---
+++ src/main/scala/hello/Hello.scala
@@ -0,0 +1,22 @@
+package hello
+
+// "containers for code" are class object or trait
+// containers contain methods (aka functions)
+// and other stuff (including other containers)
+
+// JVM convention is that a file called Xyz.scala contains a "container"
+// with the same base name
+
+object Hello {
+  // program entry point
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+
+    // declare two variables, one represents a fahrenheit temperature
+    // initialize the first one...
+    // calculate... + - * /  assignment / initialization with =
+    // calculate a celcius temperature, and print it out
+
+    // c = 5/9 of (f - 32)
+  }
+}
